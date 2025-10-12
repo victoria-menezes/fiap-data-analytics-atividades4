70852bfac7eb7d8a6d62d62d83f0ef1487f310a2
victoria-menezes <victoria.c.menezes@gmail.com>
2025-10-12 09:13:58 -0300

    First commit

---
+++ queries/preco_combustivel/create_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE anp.preco_combustivel(
+    regiao varchar(255),
+    estado varchar(255),
+    municipio varchar(255),
+    revenda varchar(255),
+    cnpj varchar(255),
+    nome_rua varchar(255),
+    numero_rua varchar(255),
+    complemento varchar(255),
+    bairro varchar(255),
+    cep varchar(255),
+    produto varchar(255),
+    data_coleta date,
+    valor_venda float,
+    unidade_medida varchar(255),
+    bandeira varchar(255)
+)
